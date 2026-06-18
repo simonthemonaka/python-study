202466189f4f5647dd68b827b4f487a29be3385c
simon <s1m0n.gap.io@gmail.com>
2026-06-18 21:40:04 +0900

    class-init

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ class foo:
 a = foo
 a.x = 123
 print(a.x)
+
+import math
+class point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def distance(self, p):
+        dx = self.x - p.x
+        dy = self.y - p.y
+        return math.sqrt(dx * dx + dy * dy)
+    
+p1 = point(0, 0)
+p2 = point(10, 10)
+print(p1.x, p2.y)
+print(p1.distance(p2))
