4f04a24efb87aeeead398d36bbfcada142a07344
simon <s1m0n.gap.io@gmail.com>
2026-06-19 11:01:03 +0900

    exception

--- python-study/mul-inh.py
+++ python-study/mul-inh.py
@@ -13,6 +13,19 @@ class C(A, B):
         super().method()
         print("C")
 
-print(C.mro())
+print(C.mro()) 
+#MRO:C->A->B したがってmethod()の呼び出しは逆になる
 c = C()
 c.method()
+
+
+###例外処理 exception is class : inheritance BaseException
+def foo(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print('Error {} / {}'.format(a, b))
+        return 0
+
+print(foo(10, 2))
+print(foo(1, 0))
